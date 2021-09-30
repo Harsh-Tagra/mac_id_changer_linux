6550a9fd725a5e13e586c68d60c5fb30bb730f16
SAHIL TAGRA <harshtagra905@gmail.com>
2021-09-30 12:11:00 +0530

    intial commit

---
+++ macIdChanger.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+service NetworkManager stop
+ macchanger -e wlan0
+ macchanger -e wlan0
+
+
+ macchanger -e wlan0
+  macchanger -e eth0 
+  macchanger -e eth0 
+  macchanger -e eth0   
+service NetworkManager start 
+exit
+
+
